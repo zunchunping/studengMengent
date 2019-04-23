dc265f00cc84f1414d441d49ddc20a76206ebe40
zunchunping <2998134412@qq.com>
2019-04-23 20:43:00 +0800

    studentManerger

---
+++ src/controller/testController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("test")
+public class testController {
+
+    @RequestMapping("index")
+    public String show(){
+
+        return "hello";
+    }
+
+}
---
+++ web/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: acer
+  Date: 2019.04.23
+  Time: 20:14
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+微软体育
+</body>
+</html>
